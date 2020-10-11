ba29c4a72165ee357343463d9fd45ba1f4e8ad49
Viklysx <vikbelec111@yandex.ru>
2020-10-11 20:23:46 +0300

    feat: task solution "Count cats" and "Carbon dating"

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
+const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (typeof(sampleActivity) === 'string' && sampleActivity.length !== 0 && ((parseFloat(sampleActivity) < 15) && parseFloat(sampleActivity) > 0)) {
+    const k = 0.693 / HALF_LIFE_PERIOD;
+    let t = Math.log((MODERN_ACTIVITY / parseFloat(sampleActivity)) / k);
+    t = Math.ceil(t);
+    if (t > 0) return t;
+  }   
+  return false;
 };
